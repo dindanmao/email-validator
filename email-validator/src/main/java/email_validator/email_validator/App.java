0a4f48a981898d98ad61ffb3ab201dc7b6148f28
Jilili <dindanmao@hotmail.com>
2017-03-01 18:35:41 -0400

    init commit

---
+++ email-validator/src/main/java/email_validator/email_validator/App.java
@@ -0,0 +1,13 @@
+package email_validator.email_validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
